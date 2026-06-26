70ee70e2e696c918bd2c346cfa62830598c7c2bf
SnO2WMaN <me@sno2wman.net>
2026-06-26 18:59:37 +0900

    Add `gotrevor/Foundation` dep

--- SeqPL.lean
+++ SeqPL.lean
@@ -1,9 +1,17 @@
-module  -- shake: keep-all --deprecated_module: ignore
+module  -- shake: keep-all
 
+public import SeqPL.Arithmetic.Interpret
+public import SeqPL.Classification.LetterlessTrace
 public import SeqPL.Formula
+public import SeqPL.Formula.Letterless
 public import SeqPL.Gentzen.Basic
 public import SeqPL.Gentzen.WithCut
 public import SeqPL.Hilbert.Basic
 public import SeqPL.Kripke.Basic
 public import SeqPL.Kripke.Gentzen
+public import SeqPL.Kripke.Rank
+public import SeqPL.Kripke.RootExtension
+public import SeqPL.Kripke.Tail
 public import SeqPL.Logic.Basic
+public import SeqPL.Logic.SumQuasiNormal
+public import SeqPL.Vorspiel.CWF
